5882c56c3aa176009534875f5c7d204ef862990b
Alyukin <awesome.alyukin@gmail.com>
2025-03-12 23:46:02 +0300

    Site-header

--- README.txt
+++ README.txt
@@ -5,13 +5,25 @@ Figma: https://www.figma.com/design/98PnkqLzcj2FkELtkyMXvo/%D0%98%D1%81%D1%82%D0
 Улучшение качества: https://snapedit.app/ru/enhance
 
 https://photokit.com/editor/?lang=ru
-Sites examples: https://sitesee.co/
-
-
-
+Sites examples: 
+https://sitesee.co/
+https://team.itmo.ru/
+https://hp.itmo.ru/
+https://science.itmo.ru/
+https://show.itmo.ru/ru/pdf_list/
+https://itmo.ru/promo/icons/pink-line.svg
+https://innopolis.university/about/
+https://etu.ru/ru/abiturientam/fotografii-i-video/
+https://etu.ru/ru/universitet/firmennyj-stil
+https://itmo.ru/ru/page/211/istoriya_universiteta_itmo.htm
 
 Options
 Background color: #F7F7F5
+Text: 
+#333
+font-size: 14px;
+line-height: 1.42857143;
+color: #333;
 
 Sources:
 GitHub: https://youtu.be/2qfyLfXEPzs?si=cw1NJ0tX4g4MVMOI
--- leti-project/src/App.vue
+++ leti-project/src/App.vue
@@ -1,12 +1,28 @@
 <script setup></script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-    <header>
-      <div>
-        <img src="/logo.png" alt="Logo" />
+  <div class="bg-[#B7FBFE]">
+    <header class="flex items-center justify-between">
+      <div class="pt-10 pl-15">
+        <img class="pointer-events-none h-45" src="/logo.png" alt="Logo" />
+      </div>
+      <div class="flex space-x-30 mb-17">
+        <ul>
+          <div class="absolute inset-x-150 h-0.5 bg-blue-300"></div>
+          <li class="text-[1.8rem] cursor-pointer hover:text-purple-500 mt-2">История</li>
+        </ul>
+        <ul>
+          <li class="text-[1.8rem] cursor-pointer hover:text-blue-500 mt-2">Университет</li>
+        </ul>
+        <ul>
+          <li class="text-[1.8rem] cursor-pointer hover:text-green-500 mt-2">Викторины</li>
+        </ul>
+      </div>
+      <div class="flex items-center space-x-2 pr-10 mb-25">
+        <img class="pointer-events-none h-15" src="/avatar.png" alt="Avatar" />
+        <img class="pointer-events-none h-15" src="/avatar.png" alt="Avatar" />
+        <img class="pointer-events-none h-15" src="/avatar.png" alt="Avatar" />
       </div>
     </header>
-    <h1>LETI History</h1>
   </div>
 </template>
