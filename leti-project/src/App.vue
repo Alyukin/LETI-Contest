2acd0285c1b03b106422d7c3b74aac7e0f5d6e56
Alyukin <awesome.alyukin@gmail.com>
2025-03-19 15:11:47 +0300

    19marchFullSize

--- leti-project/src/App.vue
+++ leti-project/src/App.vue
@@ -1,28 +1,38 @@
 <script setup></script>
 
 <template>
-  <div class="bg-[#B7FBFE]">
-    <header class="flex items-center justify-between">
-      <div class="pt-10 pl-15">
-        <img class="pointer-events-none h-45" src="/logo.png" alt="Logo" />
+  <div class="bg-[#B7FBFE] pb-200">
+    <header class="flex items-center justify-around">
+      <div class="mt-6 flex-shrink-0 w-49">
+        <img class="pointer-events-none h-40 object-contain" src="/logo.png" alt="Logo" />
       </div>
-      <div class="flex space-x-30 mb-17">
-        <ul>
-          <div class="absolute inset-x-150 h-0.5 bg-blue-300"></div>
-          <li class="text-[1.8rem] cursor-pointer hover:text-purple-500 mt-2">История</li>
-        </ul>
-        <ul>
-          <li class="text-[1.8rem] cursor-pointer hover:text-blue-500 mt-2">Университет</li>
-        </ul>
-        <ul>
-          <li class="text-[1.8rem] cursor-pointer hover:text-green-500 mt-2">Викторины</li>
-        </ul>
+      <div class="mb-10 border-t-2 border-blue-300">
+        <div class="flex space-x-20 mx-5">
+          <ul>
+            <li class="text-[1.8rem] cursor-pointer hover:text-red-500 mt-2">История</li>
+          </ul>
+          <ul>
+            <li class="text-[1.8rem] cursor-pointer hover:text-blue-500 mt-2">Университет</li>
+          </ul>
+          <ul>
+            <li class="text-[1.8rem] cursor-pointer hover:text-green-500 mt-2">Викторины</li>
+          </ul>
+        </div>
       </div>
-      <div class="flex items-center space-x-2 pr-10 mb-25">
+      <div class="flex items-center space-x-2 w-49 mb-25 mt-8">
         <img class="pointer-events-none h-15" src="/avatar.png" alt="Avatar" />
         <img class="pointer-events-none h-15" src="/avatar.png" alt="Avatar" />
         <img class="pointer-events-none h-15" src="/avatar.png" alt="Avatar" />
       </div>
     </header>
+    <img class="pointer-events-none" src="/Vector.svg" alt="Line" />
+    <div
+      class="box-border relative mx-auto px-18 pt-5 mt-10 w-[770px] h-[359px] bg-[#ffffff80] rounded-[50px]"
+    >
+      <h1 class="text-[#333333] font-medium text-[68px] leading-[127%]">
+        Виртуальный <br />тур <br />по истории ЛЭТИ
+      </h1>
+      <p class="text-[#333333] font-light text-[48px]">Узнай новое!</p>
+    </div>
   </div>
 </template>
